fd9a6780de29532e4e89267fc6f0f741be0f086b
Harshit Kumar <harshitkumar9030@gmail.com>
2024-07-01 13:15:52 +0530

    .

--- server.js
+++ server.js
@@ -16,12 +16,24 @@ mongoose.connect(process.env.MONGODB_URI, {
 const corsOptions = {
   origin: '*', 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  preflightContinue: false,
   credentials: true,
   optionsSuccessStatus: 204
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Change * to specific origin in production
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
+
+const DOWNLOADS_FOLDER = path.join(__dirname, '/home/harshit/cachedServer/videos');
+app.use('/videos', express.static(DOWNLOADS_FOLDER));
 
 app.use('/api', downloadRoutes);
 app.use('/api', trendRoutes);
