1cfefe3b23d3cc850b5a0db8b759b32fc5069182
Harshit Kumar <harshitkumar9030@gmail.com>
2024-07-13 17:10:42 +0530

    .

--- controllers/getVideosController.js
+++ controllers/getVideosController.js
@@ -1,15 +1,23 @@
 const fs = require('fs-extra');
 const path = require('path');
+const Video = require('../models/video');
 
 const DOWNLOADS_FOLDER = path.join(__dirname, '../videos');
 
 exports.getVideos = async (req, res) => {
   try {
     const files = await fs.readdir(DOWNLOADS_FOLDER);
-    const videos = files.map(file => ({
-      name: file,
-      path: path.join(DOWNLOADS_FOLDER, file)
+    const videos = await Promise.all(files.map(async (file) => {
+      const filePath = path.join(DOWNLOADS_FOLDER, file);
+      const video = await Video.findOne({ name: file }).exec();
+      return {
+        name: file,
+        path: filePath,
+        creatorName: video ? video.creatorName : 'Unknown Creator',
+        thumbnail: video ? video.thumbnail : null
+      };
     }));
+
     res.json(videos);
   } catch (error) {
     console.error(`Error retrieving videos: ${error.message}`);
