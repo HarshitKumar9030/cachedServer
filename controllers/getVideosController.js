79e6b8480845a5699592b9344cf82aa73b450f22
Harshit Kumar <harshitkumar9030@gmail.com>
2024-07-14 12:14:04 +0530

    .

--- controllers/getVideosController.js
+++ controllers/getVideosController.js
@@ -7,16 +7,31 @@ const DOWNLOADS_FOLDER = path.join(__dirname, '../videos');
 exports.getVideos = async (req, res) => {
   try {
     const files = await fs.readdir(DOWNLOADS_FOLDER);
-    const videos = await Promise.all(files.map(async (file) => {
-      const filePath = path.join(DOWNLOADS_FOLDER, file);
-      const video = await Video.findOne({ filePath }).exec();
-      return {
-        name: file,
-        path: filePath,
-        creatorName: video ? video.creatorName : 'Unknown Creator',
-        thumbnail: video ? video.thumbnail : null
-      };
-    }));
+    
+    if (files.length === 0) {
+      return res.json([]);
+    }
+
+    const videos = await Promise.all(
+      files.map(async (file) => {
+        const filePath = path.join(DOWNLOADS_FOLDER, file);
+        let video;
+        
+        try {
+          video = await Video.findOne({ filePath }).exec();
+        } catch (err) {
+          console.error(`Error finding video in DB for file ${file}: ${err.message}`);
+          video = null;
+        }
+
+        return {
+          name: file,
+          path: filePath,
+          creatorName: video ? video.creatorName : 'Unknown Creator',
+          thumbnail: video ? video.thumbnail : null
+        };
+      })
+    );
 
     res.json(videos);
   } catch (error) {
