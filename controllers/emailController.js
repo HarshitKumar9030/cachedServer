eb1cdf19cd80c1f38df6eae5a746ec946716747a
Harshit Kumar <harshitkumar9030@gmail.com>
2024-07-25 15:49:23 +0530

    chore: Add admin email notification for new contact requests

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,5 +1,6 @@
 const nodemailer = require('nodemailer');
 const generateEmailTemplate = require('../utils/emailTemplate');
+const adminTemplate = require('../utils/emailTemplate');
 
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
@@ -21,8 +22,15 @@ const sendContactEmail = async (req, res) => {
     html: generateEmailTemplate(name, email, message),
   };
 
+  const mailOptionsAdmin = {
+    from: process.env.SMTP_FROM,
+    to: process.env.SMTP_ADMIN,
+    subject: 'New Contact Request!',
+    html: adminTemplate(name, email, message),
+  };
   try {
     await transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOptionsAdmin);
     res.status(200).json({ message: 'Email sent successfully' });
   } catch (error) {
     res.status(500).json({ message: 'Error sending email', error: error.toString() });
